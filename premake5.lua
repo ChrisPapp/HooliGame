a555f13f93131c478dfb88613940320b1aabc9b5
Christos Pappas <chrispappas99@yahoo.gr>
2021-01-01 22:21:37 +0200

    Create project files
    #HappyNewYear

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+workspace "Hooligame"
+    configurations{"Debug", "Release"}
+    architecture "x86_64"
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}"
+
+project "Game"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir("bin/" .. outputdir)
+    objdir ("bin/obj/" .. outputdir)
+
+    files { "src/**.h", "src/**.cpp"}
+    includedirs { "src"}
+
+    -- Build SGG
+    if os.host() == "windows" then
+        os.execute "cd vendor\\sgg && build_sgg_x64.bat"
+        -- Copy dlls on windows
+        prebuildcommands {
+            "{COPY} vendor/sgg/3rdparty/bin/*.dll %{cfg.targetdir}"
+        }
+    elseif os.host() == "linux" then
+        os.execute "vendor/sgg/build_sgg_x64.sh"
+    else
+        os.execute "vendor/sgg/build_sgg_macOS.sh" 
+    end
+
+    includedirs {
+        "vendor/sgg/graphics/"
+    }
+    libdirs { "vendor/sgg/lib/",}
+
+    filter "configurations:Debug"
+        defines "DEBUG"
+        runtime "Debug"
+        symbols "On"
+        links { "sggd" }
+
+    filter "configurations:Release"
+        defines "RELEASE"
+        runtime "Release"
+        optimize "On"
+        links { "sgg" }
