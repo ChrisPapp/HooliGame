fa0724205b42be945e320257e0485507388f1985
Christos Pappas <chrispappas99@yahoo.gr>
2021-01-02 13:47:45 +0200

    Remove duplicate code

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,19 @@ project "Game"
     targetdir("bin/" .. outputdir)
     objdir("bin/obj/" .. outputdir)
 
+    files { "src/**.h", "src/**.cpp" }
+    includedirs { "src", "vendor/sgg/graphics/" }
+    libdirs { "vendor/sgg/lib/" }
+
+    filter "configurations:Debug"
+        defines "DEBUG"
+        runtime "Debug"
+        symbols "On"
+    filter "configurations:Release"
+        defines "RELEASE"
+        runtime "Release"
+        optimize "On"
+
     if os.host() == "windows" then
         -- build sgg
         os.execute "cd vendor\\sgg && build_sgg_x64.bat"
@@ -19,39 +32,19 @@ project "Game"
             "{COPY} vendor/sgg/3rdparty/bin/*.dll %{cfg.targetdir}"
         }
 
-        files { "src/**.h", "src/**.cpp" }
-        includedirs { "src", "vendor/sgg/graphics/" }
-        libdirs { "vendor/sgg/lib/" }
-
         filter "configurations:Debug"
-            defines "DEBUG"
-            runtime "Debug"
-            symbols "On"
             links { "sggd" }
 
         filter "configurations:Release"
-            defines "RELEASE"
-            runtime "Release"
-            optimize "On"
             links { "sgg" }
 
     elseif os.host() == "darwin" then
         os.execute "cd vendor/sgg; ./build_sgg_x64.sh"
 
-        files { "src/**.h", "src/**.cpp" }
-        includedirs { "src", "vendor/sgg/graphics/" }
-        libdirs { "vendor/sgg/lib/" }
-
         filter "configurations:Debug"
-            defines "DEBUG"
-            runtime "Debug"
-            symbols "On"
             links { "sggd", "GLEW", "SDL2", "SDL2_mixer", "freetype", "framework.dll", "OpenGL.framework" }
 
         filter "configurations:Release"
-            defines "RELEASE"
-            runtime "Release"
-            optimize "On"
             links { "sgg", "GLEW", "SDL2", "SDL2_mixer", "freetype", "framework.dll", "OpenGL.framework" }
     end
 
