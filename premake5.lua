d8f17fe4206c154031a517438d73f99e20593992
pvarouktsis <p.varouktsis@gmail.com>
2021-01-02 06:05:20 +0200

    Fix macos support

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,5 @@
 workspace "Hooligame"
-    configurations{"Debug", "Release"}
+    configurations { "Debug", "Release" }
     architecture "x86_64"
 
     outputdir = "%{cfg.buildcfg}-%{cfg.system}"
@@ -8,35 +8,50 @@ project "Game"
     kind "ConsoleApp"
     language "C++"
     targetdir("bin/" .. outputdir)
-    objdir ("bin/obj/" .. outputdir)
+    objdir("bin/obj/" .. outputdir)
 
-    files { "src/**.h", "src/**.cpp"}
-    includedirs { "src"}
-
-    -- Build SGG
     if os.host() == "windows" then
+        -- build sgg
         os.execute "cd vendor\\sgg && build_sgg_x64.bat"
-        -- Copy dlls on windows
+
+        -- copy dll on windows
         prebuildcommands {
             "{COPY} vendor/sgg/3rdparty/bin/*.dll %{cfg.targetdir}"
         }
-    elseif os.host() == "linux" then
+
+        files { "src/**.h", "src/**.cpp" }
+        includedirs { "src", "vendor/sgg/graphics/" }
+        libdirs { "vendor/sgg/lib/" }
+
+        filter "configurations:Debug"
+            defines "DEBUG"
+            runtime "Debug"
+            symbols "On"
+            links { "sggd" }
+
+        filter "configurations:Release"
+            defines "RELEASE"
+            runtime "Release"
+            optimize "On"
+            links { "sgg" }
+
+    elseif os.host() == "darwin" then
         os.execute "cd vendor/sgg; ./build_sgg_x64.sh"
-    else
-        os.execute "cd vendor/sgg; ./build_sgg_macOS.sh" 
+
+        files { "src/**.h", "src/**.cpp" }
+        includedirs { "src", "vendor/sgg/graphics/" }
+        libdirs { "vendor/sgg/lib/" }
+
+        filter "configurations:Debug"
+            defines "DEBUG"
+            runtime "Debug"
+            symbols "On"
+            links { "sggd", "GLEW", "SDL2", "SDL2_mixer", "freetype", "framework.dll", "OpenGL.framework" }
+
+        filter "configurations:Release"
+            defines "RELEASE"
+            runtime "Release"
+            optimize "On"
+            links { "sgg", "GLEW", "SDL2", "SDL2_mixer", "freetype", "framework.dll", "OpenGL.framework" }
     end
 
-    includedirs { "vendor/sgg/graphics/" }
-    libdirs { "vendor/sgg/lib/" }
-
-    filter "configurations:Debug"
-        defines "DEBUG"
-        runtime "Debug"
-        symbols "On"
-        links { "sggd" }
-
-    filter "configurations:Release"
-        defines "RELEASE"
-        runtime "Release"
-        optimize "On"
-        links { "sgg" }
